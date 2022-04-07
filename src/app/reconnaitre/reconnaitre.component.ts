009ad32809095e739448b7a3403ab767521fdf3b
Naceroui <nacer6259@hotmail.fr>
2022-04-07 14:09:58 +0200

    reco v2

--- src/app/reconnaitre/reconnaitre.component.ts
+++ src/app/reconnaitre/reconnaitre.component.ts
@@ -8,21 +8,64 @@ import { Component, OnInit,ViewChild,ChangeDetectorRef } from '@angular/core';
 })
 export class ReconnaitreComponent implements OnInit {
   constructor() { }
-  clicked = false;
-  variable : String = "../../assets/lion.jpg";
-  liste : string[] =["Lion","AZE","test","pas"];
-  compteur = 0 ;
+  clicked = false; //Le boutton n'est pas désactiver
+  liste_image : String[] = ["../../assets/lion.jpg","../../assets/chat.jpg","../../assets/chien.jpeg","../../assets/souris.jpg"];
+  
+  prochaine_image = 0;
+   //Variable qui contient l'image a trouver
+  liste_mot : string[] =["Lion","AZE","chat","pas","test2","4","5","6","8","9"]; //Liste qui contient les noms des images 
+  compteur = 0 ; //Compte le nombre d'erreur
+  variable : String = this.liste_image[this.prochaine_image];
   ngOnInit(): void {
+    this.alea(this.liste_image);
+    console.log(this.liste_image);
+    this.variable = this.liste_image[this.prochaine_image];
+
+    
   }
-  change($event: MouseEvent,varia:string):Boolean{
-    if(varia == "Lion"){
+  
+  alea(li : String[]):void{
+    var m = li.length, t, i;
+  
+    // While there remain elements to shuffle
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = li[m];
+      li[m] = li[i];
+      li[i] = t;
+    }
+  
+    
+  
+  }
+  change($event: any,varia:string):Boolean{
+    this.variable=this.liste_image[this.prochaine_image];
+    if(varia == "Lion"){ //Si reponse trouver alert un message et bloque tous les bouttons
+      alert("Bien joué");
+      this.clicked=false;
+      this.prochaine_image+=1;
+      if(this.prochaine_image < this.liste_image.length){
+        for(var i of this.liste_mot){
+          (<HTMLButtonElement>document.getElementById(i)).disabled = false;
+
+        }
+        this.variable=this.liste_image[this.prochaine_image];
+        
+        //($event.target as HTMLButtonElement).disabled = false;
+      }
+      else{
+        this.clicked=true;
+      }
       return true;
     }
-    else{
-      document.getElementById(varia)!.style.backgroundColor="red";
+    else{ //Transforme le boutton et le désactive et incrémente le nombre d'erreurs
+      //document.getElementById(varia)!.style.backgroundColor="red";
       ($event.target as HTMLButtonElement).disabled = true;
       this.compteur+=1;
-      var button = document.getElementById(varia);
+     
       console.log(this.compteur);
       return false;
     }
