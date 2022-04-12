f12b56f766bd494bb787b5106383352f46633a5a
Naceroui <nacer6259@hotmail.fr>
2022-04-12 12:18:24 +0200

    a

--- src/jeux.php
+++ src/jeux.php
@@ -1,11 +1,12 @@
 <?php
+
 class Operations
 {
  
-    private $db_host = 'localhost:8080';
+    private $db_host = 'http://92.154.61.105:8080';
     private $db_name = 'Application';
-    private $db_username = 'user';
-    private $db_password = 'Azerty$9';
+    private $db_username = 'root';
+    private $db_password = '';
  
  
     public function dbConnection()
@@ -14,11 +15,14 @@ class Operations
         try {
             $conn = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name, $this->db_username, $this->db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "aa";
             return $conn;
         } catch (PDOException $e) {
             echo "Connection error " . $e->getMessage();
             exit;
         }
     }
+
 }
+dbConnection();
 ?>
