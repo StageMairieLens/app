90d91831c1a6decb2fd3f89c1aec0f61dd1877a7
Naceroui <nacer6259@hotmail.fr>
2022-04-13 14:13:47 +0200

    t

--- src/app/reconnaitre/Reconnaitre.ts
+++ src/app/reconnaitre/Reconnaitre.ts
@@ -5,7 +5,7 @@ import { text } from 'express';
 export class Reconnaitre {
   id: number;
   static nb: number = 0;
-  liste_images: Image[];
+  images: Image[];
   bg_color: string;
   title_color: string;
 
@@ -21,9 +21,9 @@ export class Reconnaitre {
 
   typeEcriture: string;
   taille = 0;
-  constructor(liste_images: Image[],bg_color: string, title_color: string, text_color: string, good_answer_color: string, wrong_answer_color: string, color_progress_bar: Progress, button_bg_color: string, button_text_color: string, typeEcriture: string,taille:number) {
+  constructor(liste_images: Image[],bg_color: string, title_color: string, text_color: string, good_answer_color: string, wrong_answer_color: string, color_progress_bar: Progress, button_bg_color: string, button_text_color: string, typeEcriture: string) {
     this.id = Reconnaitre.nb++;
-    this.liste_images = liste_images;
+    this.images = liste_images;
     this.bg_color = bg_color;
     this.title_color = title_color;
     this.good_answer_color = good_answer_color;
@@ -33,7 +33,6 @@ export class Reconnaitre {
     this.button_text_color = button_text_color;
     this.text_color = text_color;
     this.typeEcriture = typeEcriture;
-    this.taille=taille;
   }
   
 
