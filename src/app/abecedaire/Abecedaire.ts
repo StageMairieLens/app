98485e5aa41d3197fab142ec77c36ae8dcd6479b
MichaelValet <michael.valet62@gmail.com>
2022-04-13 10:25:20 +0200

    update abecedaire

---
+++ src/app/abecedaire/Abecedaire.ts
@@ -0,0 +1,37 @@
+import { Image } from '../Image'
+import { Progress } from '../Progress'
+
+export class Abecedaire {
+    id: number;
+    static nb: number = 0;
+  
+    images: Image[];
+    bg_color: string;
+    text_color: string;
+    title_color: string;
+  
+    good_answer_color: string;
+    wrong_answer_color: string;
+  
+    button_bg_color: string;
+    button_text_color: string;
+  
+    color_progress_bar: Progress;
+  
+    typeEcriture: string;
+  
+    constructor(images: Image[], bg_color: string, title_color: string, text_color: string, good_answer_color: string, wrong_answer_color: string, color_progress_bar: Progress, button_bg_color: string, button_text_color: string, typeEcriture: string) {
+      this.id = Abecedaire.nb++;
+      this.images = images;
+      this.bg_color = bg_color;
+      this.text_color = text_color;
+      this.title_color = title_color;
+      this.good_answer_color = good_answer_color;
+      this.wrong_answer_color = wrong_answer_color;
+      this.color_progress_bar = color_progress_bar;
+      this.button_bg_color = button_bg_color;
+      this.button_text_color = button_text_color;
+      this.typeEcriture = typeEcriture;
+    }
+  
+  }
