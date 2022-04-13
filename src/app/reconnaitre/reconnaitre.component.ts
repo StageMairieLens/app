3d96cdb54010ecbf073a4d4cfa499b8d221236f5
ThibautCptr <carpentier.thibaut62@hotmail.fr>
2022-04-13 12:05:48 +0200

    update

--- src/app/reconnaitre/reconnaitre.component.ts
+++ src/app/reconnaitre/reconnaitre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild,Input } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Reconnaitre } from './Reconnaitre'
 import { Progress } from '../Progress'
 import { Image } from '../Image'
@@ -12,22 +12,23 @@ import { Image } from '../Image'
 export class ReconnaitreComponent implements OnInit {
 
   constructor() {
-    this.r = null;
+    // this.r = null;
+    this.r = new Reconnaitre([new Image('Fleur', '../../assets/fleur.jpg')], 'blue', 'white', 'black', 'green', 'red', Progress.Red, 'lightblue', 'white', 'CAPITAL');
     //this.alea(this.liste_images);
     //this.alea2(this.liste_mot_boutton);
   }
   ngOnInit(): void {
     //Zthis.alea(this.liste_images);
-    console.log(this.liste_images);
+    // console.log(this.liste_images);
     this.variable = this.liste_images[this.prochaine_image];
     this.alea(this.liste_images);
-    this.alea2(this.liste_mot_boutton);
+    // this.alea2(thiS.liste_mot_boutton);
 
 
   }
   typeEcriture: string = "CAPITAL"; // default
   @Input() r: Reconnaitre | null;
-  @Input() showTitle : boolean = true;
+  @Input() showTitle: boolean = true;
   clicked = false; //Le boutton n'est pas désactiver
   //liste_images : String[] = ["../../assets/lion.jpg","../../assets/chat.jpg","../../assets/chien.jpeg","../../assets/souris.jpg"];
   images: Image[] = [
@@ -40,21 +41,21 @@ export class ReconnaitreComponent implements OnInit {
   ];
   liste_images = [];
   prochaine_image = 0;
-   //Variable qui contient l'image a trouver
-  liste_mot : string[] = []; //Liste qui contient les noms des images
-  liste_mot_boutton : string[] = [];
-  compteur = 0 ; //Compte le nombre d'erreur
-  variable : string = this.liste_images[this.prochaine_image];
-  
+  //Variable qui contient l'image a trouver
+  liste_mot: string[] = []; //Liste qui contient les noms des images
+  liste_mot_boutton: string[] = [];
+  compteur = 0; //Compte le nombre d'erreur
+  variable: string = this.liste_images[this.prochaine_image];
+
   async delay(ms: number) {
-    await new Promise<void>(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
-}
-  alea(li:string[]):void{
-    for(var taile=0;this.images[taile];taile++){
+    await new Promise<void>(resolve => setTimeout(() => resolve(), ms)).then(() => console.log("fired"));
+  }
+  alea(li: string[]): void {
+    for (var taile = 0; this.images[taile]; taile++) {
       this.liste_mot.push(this.images[taile].getNom());
       li.push(this.images[taile].getSrc());
     }
-    var m = this.liste_mot.length, t, i,t2;
+    var m = this.liste_mot.length, t, i, t2;
 
     // While there remain elements to shuffle
     while (m) {
@@ -70,11 +71,11 @@ export class ReconnaitreComponent implements OnInit {
       this.liste_mot[i] = t2;
     }
   }
-  alea2(li:string[]):void{
-    for(var taile=0;this.images[taile];taile++){
+  alea2(li: string[]): void {
+    for (var taile = 0; this.images[taile]; taile++) {
       li.push(this.liste_mot[taile]);
     }
-    var m = this.liste_mot.length, t, i,t2;
+    var m = this.liste_mot.length, t, i, t2;
 
     // While there remain elements to shuffle
     while (m) {
@@ -88,29 +89,29 @@ export class ReconnaitreComponent implements OnInit {
     }
   }
 
-  change($event: any,varia:string):Boolean{
+  change($event: any, varia: string): Boolean {
     //this.variable=this.liste_images[this.prochaine_image];
-    if(varia == this.liste_mot[this.prochaine_image]){ //Si reponse trouver alert un message et bloque tous les bouttons
-      this.clicked=false;
-      this.prochaine_image+=1;
+    if (varia == this.liste_mot[this.prochaine_image]) { //Si reponse trouver alert un message et bloque tous les bouttons
+      this.clicked = false;
+      this.prochaine_image += 1;
       document.getElementById('result')!.innerHTML = '<p style="color : green">C\'est le bon mot</p>';
 
       setTimeout(() => {
-        this.variable=this.liste_images[this.prochaine_image];
-        document.getElementById('container')!.animate([ { opacity: 1 },
+        this.variable = this.liste_images[this.prochaine_image];
+        document.getElementById('container')!.animate([{ opacity: 1 },
         { opacity: 0.1, offset: 0.7 },
-        { opacity: 1 } ],
+        { opacity: 1 }],
 
-      800);
-      },1000);
+          800);
+      }, 1000);
       setTimeout(() => {
         document.getElementById('result')!.innerHTML = '';
-        document.getElementById('progressbar')!.style.width = ((this.prochaine_image/this.liste_images.length)*100).toString() + '%';
+        document.getElementById('progressbar')!.style.width = ((this.prochaine_image / this.liste_images.length) * 100).toString() + '%';
 
       },
-      1600);
-      if(this.prochaine_image < this.liste_images.length){
-        for(var i of this.liste_mot){
+        1600);
+      if (this.prochaine_image < this.liste_images.length) {
+        for (var i of this.liste_mot) {
           (<HTMLButtonElement>document.getElementById(i)).disabled = false;
 
         }
@@ -118,22 +119,22 @@ export class ReconnaitreComponent implements OnInit {
 
         //($event.target as HTMLButtonElement).disabled = false;
       }
-      else{
-        this.clicked=true;
+      else {
+        this.clicked = true;
       }
       return true;
     }
-    else{ //Transforme le boutton et le désactive et incrémente le nombre d'erreurs
+    else { //Transforme le boutton et le désactive et incrémente le nombre d'erreurs
       //document.getElementById(varia)!.style.backgroundColor="red";
       ($event.target as HTMLButtonElement).disabled = true;
-      this.compteur+=1;
+      this.compteur += 1;
 
       document.getElementById('result')!.innerHTML = '<p style="color : red">Ce n\'est pas le bon mot</p>';
       document.getElementById('container2')?.animate([
-        { transform: 'translateX(0px)'},
-        { transform: 'translateX(-50px)'},
-        { transform: 'translateX(50px)'}
-      ], { duration : 200 }
+        { transform: 'translateX(0px)' },
+        { transform: 'translateX(-50px)' },
+        { transform: 'translateX(50px)' }
+      ], { duration: 200 }
       );
       return false;
     }
