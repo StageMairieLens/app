7949cd4f30997ed9d41248d989b668f050ff525b
Naceroui <nacer6259@hotmail.fr>
2022-04-15 06:11:26 +0200

    a

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild,Input } from '@angular/core';
+import { Component, OnInit,ViewChild,Input, ElementRef,AfterViewInit } from '@angular/core';
 import { Puzzle } from './Puzzle'
 import { Progress } from '../Progress'
 
@@ -8,17 +8,50 @@ declare function lance():any;
 @Component({
   selector: 'app-puzzle',
   templateUrl: './puzzle.component.html',
-  styleUrls: ['./puzzle.component.css']
+  styleUrls: ['./puzzle.component.css'],
+  template: `
+    <canvas #canvas width="1000" height="1000" ></canvas>
+       
+  `,
+  styles: ['canvas { border-style: solid }']
 })
 export class PuzzleComponent implements OnInit {
+  @ViewChild('visualization') visualization: ElementRef | undefined;
+  @ViewChild('img') img: ElementRef | undefined;
   
+  private context: CanvasRenderingContext2D | undefined;
+  private element: HTMLImageElement | undefined;
+
+  src: string;
+  imgWidth: number
+  imgHeight: number
   constructor() { 
     this.r = null;
+    this.imgWidth = 600;
+    this.imgHeight = 400;
+    this.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+    
+    
 
+  }
+  ngAfterViewInit() {
+    this.context = this.visualization!.nativeElement.getContext("2d");
+    this.element = this.img!.nativeElement;
+  }
+  afterLoading() {
+    this.context!.clearRect(0, 0, this.imgWidth, this.imgHeight);
+    console.log('drawImage');
+    // this prints an image element with src I gave
+    console.log(this.element);
+    this.context!.drawImage(this.element!, 0, 0, this.imgWidth, this.imgHeight);
   }
   ngOnInit(): void {
     lance();
     restart();
+    
+    /*this.ctx = this.canvas!.nativeElement.getContext('2d')as unknown as CanvasRenderingContext2D;// ou juste !
+    this.img.src='../../assets/chat.jpg';
+    this.ctx.drawImage(this.img,10,10);*/
     //this.test();
     //rules(); 
   }
@@ -26,55 +59,10 @@ export class PuzzleComponent implements OnInit {
   @Input() r: Puzzle | null;
   @Input() showTitle : boolean = true;
   
-  test(){
-    const canvas=<HTMLCanvasElement> document.getElementById("canvas");
-    const ctx= canvas.getContext("2d");
-    const cw=  canvas.width;
-    const ch=  canvas.height;
-    
-    const rows=3;
-    const cols=3;
-    
-    const img=new Image();
-    img.onload=start;
-    img.src='../../assets/chat.png';
-    function start(){
-    
-      var iw=canvas.width=img.width;
-      var ih=canvas.height=img.height;
-      var pieceWidth=iw/cols;
-      var pieceHeight=ih/rows;
-    
-      var pieces = [
-        {col:0,row:0},
-        {col:1,row:0},
-        {col:2,row:0},
-        {col:0,row:1},
-        {col:1,row:1},
-        {col:2,row:1},
-        {col:0,row:2},
-        {col:1,row:2},
-        {col:2,row:2},
-      ]
-        //shuffle(pieces);
-    
-        var i=0;
-        for(var y=0;y<rows;y++){
-        for(var x=0;x<cols;x++){
-        var p=pieces[i++];
-      ctx!.drawImage(
-        // from the original image
-        img,
-        // take the next x,y piece
-        x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight,
-        // draw it on canvas based on the shuffled pieces[] array
-        p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight
-      );
-    }}
-    
-  }
+  
+  
   }
 
  
-}
+
  
