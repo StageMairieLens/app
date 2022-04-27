d66f171c19ebbd8c8132ad950468798778e7ebfd
ThibautCptr <carpentier.thibaut62@hotmail.fr>
2022-04-27 14:55:05 +0200

    fix

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SessionsComponent} from '../sessions/sessions.component'
-import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { SessionsComponent } from '../sessions/sessions.component'
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { LoginComponent } from './login/login.component';
 
 @Component({
@@ -12,7 +12,7 @@ import { LoginComponent } from './login/login.component';
 export class IndexComponent implements OnInit {
 
   constructor(private router: Router, public login: MatDialog) {
-   }
+  }
 
   ngOnInit(): void {
   }
@@ -23,21 +23,23 @@ export class IndexComponent implements OnInit {
     });
   }
 
-  redirect() : void {
+  redirect(): void {
     this.router.navigate(['panel']);
   }
 
-  connect(id : string) : void {
-    let id_connect = +id;
-    for(let s of SessionsComponent.data) {
-      if(s.id === id_connect) {
-        this.router.navigate(['/session/' + s.id]);
+  connect(id: string): void {
+    if (id != "") {
+      let id_connect = +id;
+      for (let s of SessionsComponent.data) {
+        if (s.id === id_connect) {
+          this.router.navigate(['/session/' + s.id]);
+        }
       }
     }
   }
 
-  connectKey($event : KeyboardEvent) : void {
-    if($event.key == 'Enter') {
+  connectKey($event: KeyboardEvent): void {
+    if ($event.key == 'Enter') {
       this.connect((<HTMLInputElement>$event.target)!.value);
     }
   }
