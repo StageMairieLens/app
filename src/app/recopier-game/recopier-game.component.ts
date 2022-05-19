9a8cb4319dfdaa7854a49a6b9b78281763f3f1c9
Naceroui <nacer6259@hotmail.fr>
2022-05-19 10:17:39 +0200

    az

--- src/app/recopier-game/recopier-game.component.ts
+++ src/app/recopier-game/recopier-game.component.ts
@@ -55,10 +55,14 @@ export class RecopierGameComponent implements OnInit {
   recup(tab: any) {
     this.jeuxService.recup_recopier(tab).subscribe(data => {
       for (var i = 0; data[i] != null; i++) {
+        if(data[i].id_crea == +localStorage.getItem('id_crea')!){
+
+        
         tab.push(
           new Recopier(data[i].id_recopier, data[i].date_recopier, this.getImage(data[i].id_image), data[i].bg_color, data[i].text_color, data[i].title_color, data[i].gaw, data[i].waw, data[i].progress, data[i].bu_bg_bo, data[i].bu_text_co, data[i].i_bg_co, data[i].i_text_co, data[i].type_ecri, data[i].isVoca, data[i].id_crea)
         );
       }
+    }
 
     })
 
